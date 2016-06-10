0dc83f2e1540936de580cf747ce91cf38f4ed878
Paul Ellmaier <paul.ellmaier@tao.at>
2016-06-10 10:52:16 +0200

    version bump to 1.08

--- tao-schedule-update.php
+++ tao-schedule-update.php
@@ -4,7 +4,7 @@
  * Description: Allows you to plan changes on any post type
  * Author: TAO Software
  * Author URI: http://software.tao.at
- * Version: 1.07
+ * Version: 1.08
  * License: MIT
  */
 
